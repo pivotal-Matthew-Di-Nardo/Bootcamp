473f976e6f3ab77e512d3ef86ea36f4bcbb988c7
Matthew Di Nardo <mdinardo@pivotal.io>
2014-09-15 13:21:30 -0400

    Added JSONParser interface. Modified JsonClient's parsing method to use a supplied JSONParser.

--- src/com/pivotallabs/bootcamp/clients/JSONClient.java
+++ src/com/pivotallabs/bootcamp/clients/JSONClient.java
@@ -3,8 +3,11 @@ package com.pivotallabs.bootcamp.clients;
 import org.apache.http.client.ClientProtocolException;
 import org.json.*;
 
+import com.pivotallabs.bootcamp.parsers.JSONParser;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.lang.Exception;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -13,63 +16,97 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.util.JsonReader;
 
-public class JSONClient extends AsyncClient{
-	
-	public interface FetchJSONCallback {
-		public void onFetchJSONSuccess(final String json);
-		
-		public void onFetchJSONFail(final Exception exception);
-	}
-	
-	private static JSONClient instance;
-	
-	private JSONClient() {
-		super.setup();
-		
-	}
-	
-	public static JSONClient getInstance() {
-		
-		if (null == instance) {
-			instance = new JSONClient();
-		}
-		
-		return instance;
-	}
-	
-	public boolean fetchJSON(@NonNull final String url, @NonNull final JSONClient.FetchJSONCallback callback, boolean asynchronous) {
-		
-		//attempt to add a JSON fetch request to the thread pool.
-		
-		final Runnable task = new Runnable() {
-				@Override
-				public void run() {				
-					try {
-						String result = ((ByteArrayOutputStream)HttpRequest(url)).toString();
-						callback.onFetchJSONSuccess(result);
-					} catch (Exception e) {
-						callback.onFetchJSONFail(e);
-					}
-				}
-			};
-		
-		try {
-			
-			if (asynchronous) {
-				super.executeTaskAsync(task);
-			} else {
-				task.run();
-			}
-			
-		} catch (InterruptedException e) {
-			//task failed to be dispatched.  return false to indicate this failure.
-			return false;
-		}
-		
-		return true;
-	}
-	
-	public void parseJSON(String url, JSONClient.FetchJSONCallback callback) {
-		JsonReader reader = new JsonReader(new StringReader(url));
+public class JSONClient extends AsyncClient {
+
+    public interface FetchJSONCallback {
+        public void onFetchJSONSuccess(String json);
+        public void onFetchJSONFail(Exception exception);
+    }
+
+    public interface ParseJSONCallback {
+        public void onParseJSONSuccess (JSONObject json);
+        public void onParseJSONFail (Exception exception);
+    }
+    
+    private static JSONClient instance;
+
+    private JSONClient() {
+        super.setup();
+
+    }
+
+    public static JSONClient getInstance() {
+
+        if (null == instance) {
+            instance = new JSONClient();
+        }
+
+        return instance;
+    }
+
+    public boolean fetchJSON(@NonNull final String url,
+            @NonNull final JSONClient.FetchJSONCallback callback,
+            boolean asynchronous) {
+
+        // attempt to add a JSON fetch request to the thread pool.
+
+        final Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String result = ((ByteArrayOutputStream) HttpRequest(url)).toString();
+                    callback.onFetchJSONSuccess(result);
+                } catch (Exception e) {
+                    callback.onFetchJSONFail(e);
+                }
+            }
+        };
+
+        try {
+
+            if (asynchronous) {
+                super.executeTaskAsync(task);
+            } else {
+                task.run();
+            }
+
+        } catch (InterruptedException e) {
+            // task failed to be dispatched. return false to indicate this
+            // failure.
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean parseJSON(final InputStream jsonStream, final JSONParser parser, final JSONClient.ParseJSONCallback callback, boolean asynchronous) {
+
+	    final Runnable task = new Runnable() {
+            @Override
+            public void run() {             
+                try {
+                    final JSONObject jsonObject = parser.parse(jsonStream);
+                    
+                    callback.onParseJSONSuccess(jsonObject);
+                } catch (Exception e) {
+                    callback.onParseJSONFail(e);
+                }
+            }
+        };
+    
+        try {
+        
+            if (asynchronous) {
+                super.executeTaskAsync(task);
+            } else {
+                task.run();
+            }
+        
+        } catch (InterruptedException e) {
+            //task failed to be dispatched.  return false to indicate this failure.
+            return false;
+        }
+	    
+        return true;
 	}
 }
